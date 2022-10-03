089b8a603dd1f987a4c41df476c47508f4cbe270
Sergio Martín <smartin@orienteed.com>
2022-10-03 10:37:52 +0200

    fix: remove unused line

--- src/logs/setup.py
+++ src/logs/setup.py
@@ -7,8 +7,6 @@ if not os.path.exists("logs/tmp"):
 
 file_name = 'logs/tmp/zammad-gateway.log'
 
-logging.basicConfig(filename=file_name, filemode='w')
-
 logger = logging.getLogger("Zammad_logger")
 logger.setLevel(logging.INFO)
 
