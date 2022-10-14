3a3d4572bf65472ab97eaa371c728bc0e4c35331
jalvarez00 <jalvarez@orienteed.com>
2022-10-14 09:11:19 +0200

    fix: save log data in a specified location

--- src/logs/setup.py
+++ src/logs/setup.py
@@ -1,13 +1,15 @@
 import logging
-import time
 from logging.handlers import RotatingFileHandler
+import os
 
+if not os.path.exists("logs/tmp"):
+    os.makedirs("logs/tmp")
 
-log_file = "zammad-gateway.log"
+file_name = "logs/tmp/zammad-gateway.log"
 
 logger = logging.getLogger("Zammad_logger")
 logger.setLevel(logging.INFO)
 
 # add a rotating handler
-handler = RotatingFileHandler(log_file, maxBytes=1000000, backupCount=6)
+handler = RotatingFileHandler(file_name, maxBytes=1000000, backupCount=6)
 logger.addHandler(handler)
